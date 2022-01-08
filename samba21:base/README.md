5f9a8b8f81c3c64b00aa7e7a5c328c5c3af1d6ac
Your Name <you@example.com>
2022-01-08 03:39:16 +0100

    08.01.22 - SAMBA

--- samba21:base/README.md
+++ samba21:base/README.md
@@ -2,16 +2,16 @@
 
 ## @edt ASIX M06 2020-2021
 
-* **keshikid03/pam21:ldap** host pam per practicar *PAM* amb autenticació local
-  unix (*pam_unix.so*) i autenticació LDAP (amb *pam_ldap.so*). Utilitza el paquet
-  *nss-pam-ldapd*. Cal configurar: *ldap.conf*, *nslcd*, *nscd*, *nsswitch*.
-  L'autentitació es configura al *system-auth*.
-
-
-  Atenció, cal usar en el container --privileged per poder fer els muntatges nfs.
+* **keshikid03/samba21:base** Host de SAMBA BASE para practicar SHARES y USUARIOS SAMBA.
 
 ```
-docker run --rm --name ldap.edt.org -h ldap.edt.org --net 2hisx -p 389:389 -d keshikid03/ldap21:latest 
-docker run --rm --name pam.edt.org --hostname pam.edt.org --net 2hisx --privileged -it keshikid03/pam21:ldap
+docker build -t keshikid03/samba21:base .
+
+docker run --rm --name smb.edt.org -h smb.edt.org --net 2hisx -p 445:445 -p 139:139 --privileged -it keshikid03/samba21:base /bin/bash
+```
+
+Para complementarlo, abrir un host de LDAP:GROUP
+```
+docker run --rm --name ldap.edt.org -h ldap.edt.org --net 2hisx -p 389:389 -d keshikid03/ldap21:group
 ```
 
--- samba21:base/startup.sh
+++ samba21:base/startup.sh
@@ -1,14 +1,68 @@
 #! /bin/bash
 
+/opt/docker/install.sh && echo "Install Success 100"
+
 # Creacio Usuaris
 useradd -m -s /bin/bash unix01 # -m crea directorio y -s shell
 useradd -m -s /bin/bash unix02
 useradd -m -s /bin/bash unix03
 
+# Creacio Usuaris SAMBA
+
+useradd -m -s /bin/bash smbunix01
+useradd -m -s /bin/bash smbunix02
+useradd -m -s /bin/bash smbunix03
+
+
 # Posar Password
 echo -e "unix01\nunix01\n" | passwd unix01
 echo -e "unix02\nunix02\n" | passwd unix02
 echo -e "unix03\nunix03\n" | passwd unix03
+
+# Posar Password a Usuaris SAMBA/UNIX
+
+echo -e "smbunix01\nsmbunix01\n" | passwd smbunix01
+echo -e "smbunix02\nsmbunix02\n" | passwd smbunix02
+echo -e "smbunix03\nsmbunix03\n" | passwd smbunix03
+
+
+# Posar Password a Usuaris SAMBA
+
+echo -e "smbunix01\nsmbunix01\n" | smbpasswd -a smbunix01
+echo -e "smbunix02\nsmbunix02\n" | smbpasswd -a smbunix02
+echo -e "smbunix03\nsmbunix03\n" | smbpasswd -a smbunix03
+
+
+# MArc
+# AQUI AFEGIM EN LOCAL ELS COMPTES d'USUARI QUE PERMETREN QUE SAMBA DEIXI COMPARTIR RECURSOS
+useradd pere
+useradd pau
+useradd anna
+
+# UN COP CREATS ELS AFEGIM PQ TINGUIN ACCESSx AL SERVIDOR SAMBA 
+echo -e "pere\npere" | smbpasswd -a -s pere
+echo -e "pau\npau" | smbpasswd -a -s pau
+echo -e "anna\nanna" | smbpasswd -a -s anna
+
+
+# Afegir els grups
+
+groupadd WinAdmins
+groupadd WinUsers
+groupadd WinGuests
+groupadd WinBackupOperators
+groupadd WinRestoreOperators
+groupadd UserHomes
+
+# -g GRUP principal, -G grup secundari
+
+usermod -g WinAdmins -G WinUsers pere
+usermod -g WinBackupOperators -G WinUsers pau
+usermod -g WinRestoreOperators -G WinUsers anna
+usermod -G UserHomes pau
+usermod -G UserHomes pere
+usermod -G UserHomes anna
+
 
 # Copiar arxius necesaris
 cp /opt/docker/ldap.conf /etc/ldap/ldap.conf
@@ -21,14 +75,36 @@ cp /opt/docker/common-account /etc/pam.d/common-account
 cp /opt/docker/common-password /etc/pam.d/common-password
 cp /opt/docker/common-session /etc/pam.d/common-session
 
-
 # Engegar dimonis necesaris
 /usr/sbin/nscd
 /usr/sbin/nslcd
+/usr/sbin/smbd && echo "smb Active"
+/usr/sbin/nmbd && echo "nmb Active"
+
+# --
+
+llistaUsers="pere marta anna pau pere jordi"
+for user in $llistaUsers
+do
+  echo -e "$user\n$user" | smbpasswd -a $user
+  line=$(getent passwd $user)
+  uid=$(echo $line | cut -d: -f3)
+  gid=$(echo $line | cut -d: -f4)
+  homedir=$(echo $line | cut -d: -f6)
+  echo "$user $uid $gid $homedir"
+  if [ ! -d $homedir ]; then
+    mkdir -p $homedir
+    cp -ra /etc/skel/. $homedir
+    chown -R $uid.$gid $homedir
+  fi
+done
 
 # Asegurar que funcione
-getent passwd
-getent group
+#getent passwd
+#getent group
+
+# Verificar SAMBA
+testparm
 
 # Interactiu
 /bin/bash
